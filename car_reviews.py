5fb07925a158ecf2cf1d1643a8cec7f91fef8961
mcarciani <mirco.carciani@power.com>
2021-11-24 00:11:27 +0000

    I'm now using GridSearchCV from scikit-learn to find the best laplas smoothing factor. I have added n-grams in the preprocess function

--- car_reviews.py
+++ car_reviews.py
@@ -7,9 +7,13 @@ nltk.download('punkt')
 nltk.download('stopwords')
 from sklearn.model_selection import train_test_split   # does this shuffle the data? YES
 from sklearn.model_selection import KFold
+from sklearn.model_selection import GridSearchCV
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+
 import numpy as np
 import pandas as pd
 from nltk.corpus import stopwords
@@ -30,7 +34,7 @@ df = pd.read_csv('car-reviews.csv')
 #print(tbl)
 
 
-def preprocess(X, Y):
+def preprocess(X, Y, stemming=True, ngram=1):
     punctuation = {".", ",", ";", "?", "!"}
     ps = PorterStemmer()
     # this function takes as input a vector of reviews (X) and their sentiment (Y)
@@ -44,12 +48,19 @@ def preprocess(X, Y):
     # REMOVE PUNCTUATION, STOP WORDS AND ANY WORD WHICH CONTAINS A NUMBER
     print("Removing punctuation and stop words....")
     stopwords_set = set(stopwords.words('english'))
-    X_processed = [[ps.stem(word) for word in word_tokenize(review) if
+    if stemming:
+        X_processed = [[ps.stem(word) for word in word_tokenize(review) if
                     word not in punctuation and word not in stopwords_set and word.isalpha()] for review in X]
+    else:
+        X_processed = [[word for word in word_tokenize(review) if
+                        word not in punctuation and word not in stopwords_set and word.isalpha()] for review in X]
+
+    temp = [zip(*[review[i:] for i in range(0, ngram)]) for review in X_processed]
+    X_processed_n_gram = [[' '.join(ngram) for ngram in zip_review] for zip_review in temp]
 
     # CREATE A WORD DICTIONARY FOR EACH REVIEW
     print("Creating a review dictionary....")
-    X_feat_dict = [dict(Counter(review)) for review in X_processed]
+    X_feat_dict = [dict(Counter(review)) for review in X_processed_n_gram]
     Y_int = (Y == 'Pos').array.astype(int)
 
     return X_feat_dict, Y_int
@@ -58,54 +69,38 @@ def preprocess(X, Y):
 train_data, test_data = train_test_split(df, test_size=0.2)
 Y_train = train_data.Sentiment
 X_train = train_data.Review
-Y_test = train_data.Sentiment
-X_test = train_data.Review
+Y_test = test_data.Sentiment
+X_test = test_data.Review
 
-# PERFORM K-FOLD VALIDATION FOR MODEL SELECTION
-kf = KFold(n_splits=5)
-for train_index, test_index in kf.split(X_train):
-    X_train_kf, X_eval_kf = X_train.iloc[train_index], X_train.iloc[test_index]
-    Y_train_kf, Y_eval_kf = Y_train.iloc[train_index], Y_train.iloc[test_index]
+# CREATE THE MULTINOMIAL NAIVE BAYES MODEL
+MN_NaiveBayes = MultinomialNB()
 
-    X_train_kf_dict, Y_train_kf_int = preprocess(X_train_kf, Y_train_kf)
-    dv = DictVectorizer()
-    X_train_kf_int = dv.fit_transform(X_train_kf_dict)
-
-    # TRAIN THE MODEL
-    print("Training a Multinomial Naive Bayes Model")
-    MN_NaiveBayes = MultinomialNB()
-    MN_NaiveBayes.fit(X_train_kf_int, Y_train_kf_int)
-
-    # EVALUATE THE MODEL
-    X_eval_kf_dict, Y_eval_kf_int = preprocess(X_eval_kf, Y_eval_kf)
-    X_eval_kf_int = dv.transform(X_eval_kf_dict)
-    Y_eval_pred = MN_NaiveBayes.predict(X_eval_kf_int)
-    cnf_matrix = confusion_matrix(Y_eval_kf_int, Y_eval_pred)
-    avg_acc = sum(Y_eval_pred == Y_eval_kf_int) / len(Y_eval_pred)
-    print(f"Average accuracy = {avg_acc}")
-    print(cnf_matrix)
-
-
-# SELECT BEST MODEL
-print("Selecting the best model....")
+# PERFORM K-FOLD VALIDATED GRID SEARCH FOR MODEL SELECTION
+param = {"alpha": np.logspace(-3, 1, num=20)}
+gsCV = GridSearchCV(MN_NaiveBayes, param, scoring= 'accuracy', cv=5, verbose=3)
 X_train_dict, Y_train_int = preprocess(X_train, Y_train)
 dv = DictVectorizer()
 X_train_int = dv.fit_transform(X_train_dict)
+gsCV.fit(X_train_int, Y_train_int)
 
-# TRAIN THE MODEL ON ALL TRAINING DATA
-print("Training The final model on all training data....")
-MN_NaiveBayes = MultinomialNB()
-MN_NaiveBayes.fit(X_train_int, Y_train_int)
+# GET THE BEST MODEL
+best_param = gsCV.best_params_
+best_score = gsCV.best_score_
+best_model = gsCV.best_estimator_
 
-# EVALUATE THE MODEL PERFORMANCES ON THE TEST DATA
-print("Evaluation Model performances")
+# EVALUATE THE BEST MODEL PERFORMANCES ON THE TEST DATA
+print("Evaluation Model performances om Test Data...")
 X_test_dict, Y_test_int = preprocess(X_test, Y_test)
 X_test_dict_int = dv.transform(X_test_dict)
-Y_test_pred = MN_NaiveBayes.predict(X_test_dict_int)
+Y_test_pred = best_model.predict(X_test_dict_int)
 cnf_matrix = confusion_matrix(Y_test_int, Y_test_pred)
-avg_acc = sum(Y_test_pred == Y_test_int) / len(Y_test_pred)
+avg_acc = accuracy_score(Y_test_int, Y_test_pred)
+f1 = f1_score(Y_test_int, Y_test_pred)
 print(f"Average accuracy = {avg_acc}")
+print(f"F1 score = {f1}")
 print(cnf_matrix)
+
+
 
 
 
